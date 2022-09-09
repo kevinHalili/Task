68b408802b473b8e594b188fb487ff0598661dc0
kevinHalili <110932757+kevinHalili@users.noreply.github.com>
2022-09-09 14:39:01 +0200

    removed old comments

--- taskSpring/taskSpring/src/main/java/com/example/taskSpring/service/AppUserDataService.java
+++ taskSpring/taskSpring/src/main/java/com/example/taskSpring/service/AppUserDataService.java
@@ -98,65 +98,6 @@ public class AppUserDataService {
     }
 
 
-//    public AppUserData setUserData(Long userId)
-//    {
-//        long workingDaysId = 1;
-//        long offDaysId = 1;
-//        AppUserData appUserData = new AppUserData();
-//        AppUser user = appUserRepository.findById( userId )
-//                .orElseThrow(() -> new IllegalStateException("User with id" + userId + "does not exist"));
-//
-//
-//        appUserData.setFullName( user.getName() + " " + user.getSurname());
-//        int totalOvertime = 0;
-//        int totalRegular = 0;
-//        double totalPay = 0;
-//
-//
-//
-//        while(workingDaysRepository.existsById(workingDaysId) || offDaysRepository.existsById(offDaysId)) {
-//            WorkingDays workingDays = workingDaysRepository.findById( workingDaysId++ )
-//                    .orElseThrow(() -> new IllegalStateException("Working day does not exist"));
-//            OffDays offDays = offDaysRepository.findById( offDaysId++ )
-//                    .orElseThrow(() -> new IllegalStateException("Working day does not exist"));
-//            if(workingDays.getUser().equals( user ) ) {
-//                if (workingDays.getHours() > 8) {
-//                    appUserData.setRegularHours( 8 );
-//                    appUserData.setOvertimeHours( workingDays.getHours() - 8 );
-//                }
-//
-//                appUserData.setTotalHours( workingDays.getHours() );
-//
-//                if (workingDays.getDate().equals( offDays.getDate() ) || (workingDays.getDate().get( Calendar.DAY_OF_WEEK ) > 6
-//                        && workingDays.getDate().equals( offDays.getDate() ))) {
-//                    appUserData.setTotalPay(
-//                            user.getHourlyPayment() * (appUserData.getRegularHours() * 1.5 +
-//                                    appUserData.getOvertimeHours() * 2) );
-//                } else if (workingDays.getDate().get( Calendar.DAY_OF_WEEK ) > 6) {
-//                    appUserData.setTotalPay(
-//                            user.getHourlyPayment() * (appUserData.getRegularHours() * 1.25 +
-//                                    appUserData.getOvertimeHours() * 1.5) );
-//                } else {
-//                    appUserData.setTotalPay(
-//                            user.getHourlyPayment() * (appUserData.getRegularHours() +
-//                                    appUserData.getOvertimeHours()) );
-//                }
-//
-//                totalPay += appUserData.getTotalPay();
-//                totalOvertime += appUserData.getOvertimeHours();
-//                totalRegular += appUserData.getRegularHours();
-//            }
-//        }
-//        appUserData.setRegularHours( totalRegular );
-//        appUserData.setTotalPay( totalPay );
-//        appUserData.setOvertimeHours( totalOvertime );
-//        appUserData.setTotalHours( totalOvertime + totalRegular );
-//
-//        return  appUserDataRepository.save(appUserData);
-//
-//    }
-
-
 
 
 
